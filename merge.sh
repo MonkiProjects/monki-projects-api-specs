2682aa30ce346bc9193d8becfd96641abca2f712
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-06-02 22:17:13 +0200

    Fix `merge.sh` not working anymore for some reason

--- merge.sh
+++ merge.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+cd specs
 # This script uses [openapi-merge-cli](https://www.npmjs.com/package/openapi-merge-cli)
-npx openapi-merge-cli --config openapi-merge-v1.yaml
+npx openapi-merge-cli --config ../openapi-merge-v1.yaml

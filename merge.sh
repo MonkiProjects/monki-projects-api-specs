62616634fc3d85049ec209ac260618f2b30660d5
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-06-02 22:51:57 +0200

    Add dereferenced version of OpenAPI spec

--- merge.sh
+++ merge.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# This script uses
+# - [openapi-merge-cli](https://www.npmjs.com/package/openapi-merge-cli)
+# - [swagger-cli](https://github.com/APIDevTools/swagger-cli)
+
+# Merge files together
 cd specs
-# This script uses [openapi-merge-cli](https://www.npmjs.com/package/openapi-merge-cli)
 npx openapi-merge-cli --config ../openapi-merge-v1.yaml
+
+# Dereference merged file
+cd ..
+npx swagger-cli bundle -t yaml -o ./specs/monki-projects-api-v1-deref.yaml ./specs/monki-projects-api-v1.yaml
